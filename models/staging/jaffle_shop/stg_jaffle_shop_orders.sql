d77092059fe7a665d84070a1bb82a5b271bd4be1
Vinicius Torres <vini.guerra2@hotmail.com>
2025-05-30 23:33:42 +0000

    build: adicionar verificação de freshness para tabela orders

--- models/staging/jaffle_shop/stg_jaffle_shop_orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop_orders.sql
@@ -1,7 +1,5 @@
-select
-id as order_id,
-user_id as customer_id,
-order_date,
-status,
-_etl_loaded_at
-from `dbt-tutorial.jaffle_shop.orders`
+select id as order_id, user_id as customer_id, order_date, status, _etl_loaded_at
+from {{source("jaffle_shop","orders")}}
+
+
+--from `dbt-tutorial.jaffle_shop.orders` sem source
